776750c31b596ce72da80f1e9ac961dc9349a3c9
我的心好冷 <963010359@qq.com>
2020-09-23 15:25:04 +0800

    service provider 服务提供者

--- sp05-eureka/src/main/java/cn/tedu/sp05/Sp05EurekaApplication.java
+++ sp05-eureka/src/main/java/cn/tedu/sp05/Sp05EurekaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//启动Eureka服务器
 @EnableEurekaServer
 @SpringBootApplication
 public class Sp05EurekaApplication {
